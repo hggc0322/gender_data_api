7bbe8809a9715951c431e8f23274ecd9c235214c
TaraYoo <yootar@gmail.com>
2019-03-23 13:08:46 -0400

    Add relationships
    Add relationships between company and region

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_23_162756) do
+ActiveRecord::Schema.define(version: 2019_03_23_170048) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_03_23_162756) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "benefits"
+    t.integer "region_id"
+    t.index ["region_id"], name: "index_companies_on_region_id"
   end
 
   create_table "regions", force: :cascade do |t|
